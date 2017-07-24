13bdf1cd4a43fcb3b14aaacd071c49ca87f103a9
theSeb1979 <sebastian.me@gmail.com>
2017-07-24 10:38:39 +0100

    Make things work

--- src/main.lua
+++ src/main.lua
@@ -7,9 +7,12 @@
 -- Copyright (c) Realismus Modding, 2017
 ----------------------------------------------------------------------------------------------------
 
-
-if g_currentMission.fieldDefinitionBase ~= nil and g_currentMission.fieldDefinitionBase.fieldDefs ~= nil then
-    for _,fieldDef in pairs(g_currentMission.fieldDefinitionBase.fieldDefs) do
-        fieldDef.fieldJobUsageAllowed = false
+Mission00.loadMission00Finished = Utils.appendedFunction(Mission00.loadMission00Finished, function ()
+    -- Register the non-tabbing to all vehicles
+    if g_currentMission.fieldDefinitionBase ~= nil and g_currentMission.fieldDefinitionBase.fieldDefs ~= nil then
+        for _,fieldDef in pairs(g_currentMission.fieldDefinitionBase.fieldDefs) do
+            fieldDef.fieldJobUsageAllowed = false
+        end
     end
-end
+end)
+
