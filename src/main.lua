f1222c2dcfb7b052688051d10d270dfc36980eb8
theSeb1979 <sebastian.me@gmail.com>
2017-07-24 10:38:59 +0100

    erroneous comment

--- src/main.lua
+++ src/main.lua
@@ -8,7 +8,6 @@
 ----------------------------------------------------------------------------------------------------
 
 Mission00.loadMission00Finished = Utils.appendedFunction(Mission00.loadMission00Finished, function ()
-    -- Register the non-tabbing to all vehicles
     if g_currentMission.fieldDefinitionBase ~= nil and g_currentMission.fieldDefinitionBase.fieldDefs ~= nil then
         for _,fieldDef in pairs(g_currentMission.fieldDefinitionBase.fieldDefs) do
             fieldDef.fieldJobUsageAllowed = false
